908f4a025ff9459595639fc2b03698035ab6b2d7
Facundo Adaro <adarbox@gmail.com>
2023-07-17 17:05:35 -0300

    Cambiando fondo de pantalla dependiendo las condiciones

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ form.addEventListener('submit', (e) => {
 function callAPI(city) {
     const apiId = '31d97553eea8431b5f769cda1b7ada51'
     // const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lon=${country}&units=metric&appid=${apiId}`
-    const url = `https://api.weatherapi.com/v1/forecast.json?key=6e2e58017387443484a211202231407&q=${city}&days=3&aqi=no`
+    const url = `https://api.weatherapi.com/v1/forecast.json?key=6e2e58017387443484a211202231407&q=${city}&days=4&aqi=no`
 
     fetch(url)
         .then(res => res.json())
@@ -50,7 +50,14 @@ function callAPI(city) {
         })
 }
 
+const currentDate = new Date(); // Obtiene la fecha actual
 
+const dayNames = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado']
+const dayNum = currentDate.getDay(); // Obtiene el número del día de la semana (0 para Domingo, 1 para Lunes, etc.)
+
+const dayName = dayNames[dayNum]
+
+console.log(dayName); // Muestra el número del día de la semana correspondiente a la fecha actual
 
 function showWeather(data) {
     pais.textContent = data.location.name
@@ -58,29 +65,35 @@ function showWeather(data) {
     tempMax.textContent = data.forecast.forecastday[0].day.maxtemp_c
     tempMin.textContent = data.forecast.forecastday[0].day.mintemp_c
     humidity.textContent = data.current.humidity
+
+    data.forecast.forecastday.slice(1, 4).forEach((forecast, index) => {
+        const dateString = forecast.date;
+        const dateObj = new Date(dateString);
+        const dayNum = dateObj.getDay();
+        const dayName = dayNames[dayNum];
+        const days = document.getElementById(`nextday${index + 1}`);
+        days.textContent = dayName;
+      });
+
     for (let i = 0; i < 3; i++) {
         let temp_max = data.forecast.forecastday[i].day.maxtemp_c;
         let spanMaxElement = document.getElementById(`tempmax${i + 1}`);
         spanMaxElement.textContent = temp_max
     }
     for (let i = 0; i < 3; i++) {
-        let temp_min = data.forecast.forecastday[i].day.mintemp_c;
-        let spanMinElement = document.getElementById(`tempmin${i + 1}`);
+        let temp_min = data.forecast.forecastday[i].day.mintemp_c
+        let spanMinElement = document.getElementById(`tempmin${i + 1}`)
         spanMinElement.textContent = temp_min
     }
     for (let i = 0; i < 3; i++) {
-        let nextWind = data.forecast.forecastday[i].day.maxwind_kph;
-        let otherWind = document.getElementById(`datewind${i + 1}`);
+        let nextWind = data.forecast.forecastday[i].day.maxwind_kph
+        let otherWind = document.getElementById(`datewind${i + 1}`)
 
         otherWind.textContent = nextWind
     }
-    for (let i = 0; i < 3; i++) {
-        let nextday = data.forecast.forecastday[i].date;
-        let days = document.getElementById(`nextday${i + 1}`);
-        days.textContent = nextday
-    }
+
     if (data && data.forecast.forecastday[0].day.daily_chance_of_rain) {
-        rain.textContent = data.forecast.forecastday[0].day.daily_chance_of_rain;
+        rain.textContent = data.forecast.forecastday[0].day.daily_chance_of_rain
         // document.body.style.backgroundImage = "url('/pronostico/lluvioso.jpg')"
     } else {
         // Si no existe la propiedad o es falsa, puedes realizar alguna otra acción o dejarlo vacío.
@@ -89,12 +102,25 @@ function showWeather(data) {
     wind.textContent = data.current.wind_kph
     pressure.textContent = data.current.pressure_mb
     feelslike.textContent = data.current.feelslike_c
-    if (data.current.temp_c <= 12) {
-        document.body.style.backgroundImage = "url('/pronostico/Nevado.jpg')"
-    } else if (data && data.forecast.forecastday[0].day.daily_chance_of_rain) {
+    if(data.forecast.forecastday[0].day.daily_will_it_rain > 0){
         document.body.style.backgroundImage = "url('/pronostico/lluvioso.jpg')"
-    } else {
+        return;
+    }
+    if(data.current.temp_c <= 10 && data.forecast.forecastday[0].day.daily_will_it_rain < 1){
+        document.body.style.backgroundImage = "url('/pronostico/helada.jpg')"
+        return;
+    }
+    if(data.current.temp_c > 10 && data.current.temp_c < 18 ){
+        document.body.style.backgroundImage = "url('/pronostico/frío.jpg')"
+        return;
+    }
+    if(data.current.temp_c >= 18 && data.current.temp_c < 25){
         document.body.style.backgroundImage = "url('/pronostico/soleado.jpg')"
+        return;
+    }
+    if(data.current.temp_c >= 25){
+        document.body.style.backgroundImage = "url('/pronostico/caluroso.jpg')"
+        return;
     }
 }
 
