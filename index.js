889037159ad1f743e70c4900e4d279b5899d9821
Facundo Adaro <adarbox@gmail.com>
2023-07-17 00:04:54 -0300

    Agregando proximos dias

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const rain = document.getElementById('rain')
 const pressure = document.getElementById('pressure')
 const feelslike = document.getElementById('feels_like')
 const wind = document.getElementById('wind')
+const fmax = document.querySelector('.forecast-max')
+const fmin = document.getElementById('forecast-min')
 
 
 form.addEventListener('submit', (e) => {
@@ -30,7 +32,8 @@ form.addEventListener('submit', (e) => {
     }
 })
 
-function callAPI(city, country) {
+
+function callAPI(city) {
     const apiId = '31d97553eea8431b5f769cda1b7ada51'
     // const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lon=${country}&units=metric&appid=${apiId}`
     const url = `https://api.weatherapi.com/v1/forecast.json?key=6e2e58017387443484a211202231407&q=${city}&days=3&aqi=no`
@@ -43,7 +46,6 @@ function callAPI(city, country) {
             } else {
                 console.log(data)
                 showWeather(data)
-
             }
         })
 }
@@ -51,11 +53,32 @@ function callAPI(city, country) {
 
 
 function showWeather(data) {
-    pais.textContent = data.name
+    pais.textContent = data.location.name
     temp.textContent = data.current.temp_c
     tempMax.textContent = data.forecast.forecastday[0].day.maxtemp_c
     tempMin.textContent = data.forecast.forecastday[0].day.mintemp_c
     humidity.textContent = data.current.humidity
+    for (let i = 0; i < 3; i++) {
+        let temp_max = data.forecast.forecastday[i].day.maxtemp_c;
+        let spanMaxElement = document.getElementById(`tempmax${i + 1}`);
+        spanMaxElement.textContent = temp_max
+    }
+    for (let i = 0; i < 3; i++) {
+        let temp_min = data.forecast.forecastday[i].day.mintemp_c;
+        let spanMinElement = document.getElementById(`tempmin${i + 1}`);
+        spanMinElement.textContent = temp_min
+    }
+    for (let i = 0; i < 3; i++) {
+        let nextWind = data.forecast.forecastday[i].day.maxwind_kph;
+        let otherWind = document.getElementById(`datewind${i + 1}`);
+
+        otherWind.textContent = nextWind
+    }
+    for (let i = 0; i < 3; i++) {
+        let nextday = data.forecast.forecastday[i].date;
+        let days = document.getElementById(`nextday${i + 1}`);
+        days.textContent = nextday
+    }
     if (data && data.forecast.forecastday[0].day.daily_chance_of_rain) {
         rain.textContent = data.forecast.forecastday[0].day.daily_chance_of_rain;
         // document.body.style.backgroundImage = "url('/pronostico/lluvioso.jpg')"
@@ -68,9 +91,9 @@ function showWeather(data) {
     feelslike.textContent = data.current.feelslike_c
     if (data.current.temp_c <= 12) {
         document.body.style.backgroundImage = "url('/pronostico/Nevado.jpg')"
-    }else if(data && data.forecast.forecastday[0].day.daily_chance_of_rain){      
-            document.body.style.backgroundImage = "url('/pronostico/lluvioso.jpg')"
-    }else{
+    } else if (data && data.forecast.forecastday[0].day.daily_chance_of_rain) {
+        document.body.style.backgroundImage = "url('/pronostico/lluvioso.jpg')"
+    } else {
         document.body.style.backgroundImage = "url('/pronostico/soleado.jpg')"
     }
 }
